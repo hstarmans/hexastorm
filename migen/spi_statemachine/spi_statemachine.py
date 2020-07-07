e29dd0128c78a7a7e7196959b89596d763e69103
Rik Starmans <info@hexastorm.com>
2020-07-07 15:56:01 +0200

    added test for LED

--- migen/spi_statemachine/spi_statemachine.py
+++ migen/spi_statemachine/spi_statemachine.py
@@ -5,7 +5,7 @@
 
     Rik Starmans
 """
-import collections
+from collections import namedtuple
 
 import unittest
 from migen.fhdl import verilog
@@ -24,13 +24,19 @@ VARIABLES = {'RPM':2400,'SPINUP_TICKS':1.5,'MAX_WAIT_STABLE_TICKS':1.125, 'FACET
             'SCANLINE_DATA_SIZE':790,'TICKS_PER_PRISM_FACET':12500, 'TICKS_START':4375,
              'SINGLE_FACET':0, 'DIRECTION':0, 'JITTER_ALLOW':300, 'JITTER_THRESH':400}
 
+# TODO: ugly
+commands = ('RECVCOMMAND', 'STATUS', 'START', 'STOP', 'READ_D', 'WRITE_L')
+Commands = namedtuple('Commands', commands, defaults=tuple(range(len(commands))))
+COMMANDS = Commands()
+
 
 class SpiStateMachine(Module):
-    def __init__(self, spi_port, data_width):
-        # three submodules; SPI receiver, memory and laser state machine
+    
 
+    def __init__(self, spi_port, led):
+        # three submodules; SPI receiver, memory and laser state machine
         # full byte state
-        state      =  Signal(3)   # state laser module
+        ledstate   =  Signal(3)   # state laser module
         error      =  Signal(4)   # error state
         memoryfull =  Signal()
         
@@ -43,59 +49,52 @@ class SpiStateMachine(Module):
         #        write cannot be set equal to read address  --> writing on a line which is still in use
         # Nextwrite and nextread addres used to know when writing is finished
         # writebyte, current byte written to
-        # sram memory is 32 blocks... each block has to be handled seperately
-        # one block 16*256 = 4096 bytes
+        # sram memory is 32 blocks... each block has its own ports
+        # one block 16*256 = 4096 bytes currently used
         WIDTH = 16
         DEPTH = 256
         LINES = (WIDTH*DEPTH)//VARIABLES['SCANLINE_DATA_SIZE']
         self.specials.mem = Memory(width=WIDTH, depth=DEPTH, init = [10,20])
-        p1 = self.mem.get_port(write_capable=True, mode = READ_FIRST)
-        p2 = self.mem.get_port(has_re=True)
-        self.specials += p1, p2
-        self.ios = {p1.adr, p1.dat_w, p1.we, p2.dat_r, p2.adr, p2.re}
+        writeport = self.mem.get_port(write_capable=True, mode = READ_FIRST)
+        readport = self.mem.get_port(has_re=True)
+        self.specials += writeport, readport
+        self.ios = {writeport.adr, writeport.dat_w, writeport.we, readport.dat_r, readport.adr, readport.re}
         self.submodules.memory = FSM(reset_state = "IDLE")
         nextreadaddress = Signal(max=DEPTH)
         nextwriteaddress = Signal(max=DEPTH)
         self.writebyte = Signal(max=WIDTH)
        
-        self.comb = memoryfull.eq(nextwriteaddress==p2.adr)
+        self.comb += memoryfull.eq(nextwriteaddress==readport.adr)
 
         self.memory.act("IDLE",
             NextState("READ"),
             # read address set by laser scanner state machine
-            NextValue(p1.we, 0),
-            NextValue(p2.re, 1)
+            NextValue(writeport.we, 0),
+            NextValue(readport.re, 1)
         )
         self.memory.act("READ",
             # data read out by laser scanner state machine
             # write address and data set by spi state machine
-            NextValue(nextreadaddress, p2.adr+1),
-            NextValue(p2.re, 0),
-            NextValue(p1.we, 1),
+            NextValue(nextreadaddress, readport.adr+1),
+            NextValue(readport.re, 0),
+            NextValue(writeport.we, 1),
             NextState("WRITE")
         )
         self.memory.act("WRITE",
-            NextValue(nextwriteaddress, p1.adr+1),
-            NextValue(p1.we, 0),
+            NextValue(nextwriteaddress, writeport.adr+1),
+            NextValue(writeport.we, 0),
             NextState("IDLE"),
         )
         # Receiver State Machine
         # Consists out of component from litex and own custom component
-        # Detects wether new command is available
-        spislave = SPISlave(spi_port, data_width)
+        # Detects whether new command is available
+        spislave = SPISlave(spi_port, data_width=8)
         self.submodules.slave = spislave
 
-        # COMMANDS  --> TODO: could be moved to DICT
-        STATUS = 1
-        START = 2
-        STOP = 3
-        READ_D = 4
-        WRITE_L = 5
-        READ_L = 6
-        # The command variable contains command which will be executed
-        # Often this is the recvcommand, cannot be set externally
-        RECVCOMMAND = 0
-        command = Signal(max=6) # max is READ_L
+        # COMMANDS 
+        # The command variable contains command to be executed
+        # typically the recvcommand, cannot be set externally
+        command = Signal(max=len(COMMANDS))
         # Done detector
         done_d = Signal()
         done_rise = Signal()
@@ -109,64 +108,86 @@ class SpiStateMachine(Module):
         # Custom Receiver
         self.submodules.receiver = FSM(reset_state = "IDLE")
         self.receiver.act("IDLE",
-            NextValue(spislave.miso, Cat(state+error+memoryfull)),
+                NextValue(spislave.miso, Cat(ledstate+error+memoryfull)),
             If(start_rise,
-                NextState("WAITFORDONE")))
+                NextState("WAITFORDONE")
+            )
+        )
         self.receiver.act("WAITFORDONE",
             If(done_rise,
-                NextState("PROCESSINPUT")))
+                NextState("PROCESSINPUT")
+            )
+        )
         self.receiver.act("PROCESSINPUT",
             NextState("IDLE"),
             # Read Header
-            If(command == RECVCOMMAND,
-                If(spislave.mosi == STOP,
-                    NextValue(state, 0)
+            If(command == COMMANDS.RECVCOMMAND,
+                If(spislave.mosi == COMMANDS.STOP,
+                    NextValue(ledstate, 0)
                 ).
-                Elif(spislave.mosi == START,
-                    NextValue(state, 1)
+                Elif(spislave.mosi == COMMANDS.START,
+                    NextValue(ledstate, 1)
                 ).
-                Elif(spislave.mosi == READ_D,
-                    NextValue(command, READ_D),
+                Elif(spislave.mosi == COMMANDS.READ_D,
+                    NextValue(command, COMMANDS.READ_D),
                     NextValue(spislave.miso, debug)
                 ).
-                Elif(spislave.mosi == WRITE_L & memoryfull == 0,
-                    NextValue(command, WRITE_L),
+                Elif((spislave.mosi == COMMANDS.WRITE_L) & (memoryfull == 0),
+                    NextValue(command, COMMANDS.WRITE_L),
                     NextValue(spislave.miso, 0)
                 )
+                # Else; Command invalid or memory full nothing happens
             ).
             # Read data after header; only applicable for debug or write line
             Else(
-                If(command == READ_D,
-                    NextValue(command, RECVCOMMAND),
+                If(command == COMMANDS.READ_D,
+                    NextValue(command, COMMANDS.RECVCOMMAND),
                 ).
                 # command must be WRITE_L
                 Else(
                     # all bytes are received
                     If(self.writebyte>=WIDTH,
                         NextValue(self.writebyte,0),
-                        # Not done writing, so can't move to next address, raise error
-                        NextValue(error[0], p1.adr==nextwriteaddress),
-                        NextValue(p1.adr, p1.adr+1),
-                        NextValue(command, RECVCOMMAND)
+                        # If not done writing raise error
+                        NextValue(error[0], writeport.adr==nextwriteaddress),
+                        NextValue(writeport.adr, writeport.adr+1),
+                        NextValue(command, COMMANDS.RECVCOMMAND)
                     ).
                     Else(
-                        NextValue(p1.dat_w[self.writebyte:], spislave.mosi),
+                        NextValue(writeport.dat_w[self.writebyte.variable:], spislave.mosi),
                         NextValue(self.writebyte, self.writebyte+1),
                         NextValue(spislave.miso, 0)
                     )
                 )
             )
         )
+    
+        # LED State machine
+        self.submodules.ledfsm = FSM(reset_state = "OFF")
+        self.ledfsm.act("OFF",
+            NextValue(led, 0),
+            If(ledstate==1,
+                 NextState("ON")
+            )
+        )
+        self.ledfsm.act("ON",
+            NextValue(led, 1),
+            If(ledstate==0,
+                NextState("OFF")
+            )
+        )
 
-class TestSPI(unittest.TestCase):
-    def test_spi_slave_xfer(self):
+
+class TestSPIStateMachine(unittest.TestCase):
+    def test_getstate(self):
         class DUT(Module):
             def __init__(self):
                 pads = Record([("clk", 1), ("cs_n", 1), ("mosi", 1), ("miso", 1)])
                 self.submodules.master = SPIMaster(pads, data_width=8,
-                    sys_clk_freq=100e6, spi_clk_freq=5e6,
-                    with_csr=False)
-                self.submodules.spimemmap = SpiMemmapping(pads, 8)
+                        sys_clk_freq=100e6, spi_clk_freq=5e6,
+                        with_csr=False)
+                self.led = Signal()
+                self.submodules.spi_statemachine = SpiStateMachine(pads, self.led)
 
         def master_generator(dut):
             def transaction(data_sent, data_received):
@@ -179,41 +200,38 @@ class TestSPI(unittest.TestCase):
                 while (yield dut.master.done) == 0:
                     yield
                 self.assertEqual((yield dut.master.miso), data_received)
-            # read from address 0, check equal to 10
-            yield from transaction(2, 0)
-            yield from transaction(2, 10)
-            # change address to 1
-            yield from transaction(3, 0)
-            yield from transaction(1, 3)
-            # read from address 1, check equal to 20
-            yield from transaction(2, 0)
-            yield from transaction(2, 20)
-            # write 5 to address 1
-            yield from transaction(1, 0)
-            yield from transaction(5, 1)
-            # change address to 0
-            yield from transaction(3, 0)
-            yield from transaction(0, 3)
-            # read from address 0, check equal to 10
-            yield from transaction(2, 0)
-            yield from transaction(2, 10)
-            # change address to 1
-            yield from transaction(3, 0)
-            yield from transaction(1, 3)
-            # read from address 1, check equal to 5 
-            yield from transaction(2, 0)
-            yield from transaction(2, 5)
+            # get the initial status
+            yield from transaction(COMMANDS.STATUS, 0)
+            # turn on the LED, status should still be zero
+            yield from transaction(COMMANDS.START, 0)
+            # check wether the led is on
+            yield from transaction(COMMANDS.STATUS, 1)
+            # turn OFF the led
+            yield from transaction(COMMANDS.STOP, 1)
+            # LED should be off
+            yield from transaction(COMMANDS.STATUS, 0)
 
         def slave_generator(dut):
-            def transaction(data_received): 
-                while (yield dut.spimemmap.slave.start) == 0:
-                    yield
-                while (yield dut.spimemmap.slave.done) == 0:
-                    yield
-                self.assertEqual((yield dut.spimemmap.slave.mosi), data_received)
-                self.assertEqual((yield dut.spimemmap.slave.length), 8)
-            self.assertEqual((yield dut.spimemmap.writebyte), 0)
-            yield from transaction(2)
+            timeout = 0
+            # LED should be off on the start
+            self.assertEqual((yield dut.led), 0)
+            # wait till led state changes
+            while (yield dut.led) == 0:
+                timeout += 1
+                if timeout>1000:
+                    raise Exception("Led doesn't turn on.")
+                yield
+            timeout = 0
+            # LED should be on now
+            self.assertEqual((yield dut.led), 1)
+            # wait till led state changes
+            while (yield dut.led) == 1:
+                timeout += 1
+                if timeout>1000:
+                    raise Exception("Led doesn't turn off.")
+                yield
+            # LED should be off now
+            self.assertEqual((yield dut.led), 0)
         dut = DUT()
         run_simulation(dut, [master_generator(dut), slave_generator(dut)])
 
@@ -224,7 +242,8 @@ if __name__ == '__main__':
         if sys.argv[1] == 'build':
             plat = board.Platform()
             spi_port = plat.request("spi")
-            spi_memmapping = SpiMemmapping(spi_port, 8)
-            plat.build(spi_memmapping, build_name = 'spi_memmapping')
+            led = plat.request("user_led")
+            spi_statemachine = SpiStateMachine(spi_port, led)
+            plat.build(spi_statemachine, led, build_name = 'spi_statemachine')
     else:
         unittest.main()
