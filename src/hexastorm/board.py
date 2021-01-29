0c028fc13c53c71bef9df96ad7d35d9ec40dc3c7
Rik Starmans <info@hexastorm.com>
2021-01-29 17:39:33 +0000

    started cleaning up code with flake8

--- src/hexastorm/board.py
+++ src/hexastorm/board.py
@@ -1,5 +1,5 @@
 """
-    constraint file for the Kicad board available at 
+    constraint file for the Kicad board available at
     https://github.com/hstarmans/firestarter/tree/master/pi_hat
 """
 import subprocess
@@ -8,10 +8,9 @@ import shutil
 
 from migen.build.generic_platform import Subsignal, Pins, IOStandard
 from migen.build.lattice import LatticePlatform
-from migen.build.lattice.programmer import IceStormProgrammer
 
 _io = [
-    # numbers are for multiple channels, not really used, 
+    # numbers are for multiple channels, not really used,
     # TODO: wrap polygon in one and laser in one
     ("gpio14", 0, Pins("113"), IOStandard("LVCMOS33")),
     ("gpio15", 0, Pins("112"), IOStandard("LVCMOS33")),
@@ -25,10 +24,10 @@ _io = [
     ("poly_ready", 0, Pins("138"), IOStandard("LVCMOS33")),  # not used
     ("poly_pwm", 0, Pins("139"), IOStandard("LVCMOS33")),
     ("spi", 0,
-        Subsignal('cs_n',Pins("85")),
-        Subsignal('miso',Pins("87")),
-        Subsignal('mosi',Pins("90")),
-        Subsignal('clk',Pins("79")),
+        Subsignal('cs_n', Pins("85")),
+        Subsignal('miso', Pins("87")),
+        Subsignal('mosi', Pins("90")),
+        Subsignal('clk', Pins("79")),
         IOStandard("LVCMOS33"))
 ]
 _connectors = []
@@ -37,28 +36,28 @@ _connectors = []
 class Platform(LatticePlatform):
     default_clk_name = "clk100"
     default_clk_period = 10
-    default_clk_freq = 100 # MHz
+    default_clk_freq = 100  # MHz
 
     def __init__(self):
         LatticePlatform.__init__(self, "ice40-hx8k-tq144:4k",
                                  _io, _connectors, toolchain="icestorm")
 
     def build(self, core, build_name, freq=default_clk_freq):
-        if 'arm' in platform.machine():
-                # arm platform uses apio
-                # --pre-pack can't be used as binary is not compiled with python
-                print(f"Using apio extension with freq {freq} MHz")
-                self.toolchain.nextpnr_build_template = [
-                    'apio raw "yosys -q -l {build_name}.rpt {build_name}.ys"',
-                    'apio raw "nextpnr-ice40 {pnr_pkg_opts} --pcf {build_name}.pcf --json {build_name}.json --asc {build_name}.txt --freq '+str(freq)+'"',
-                    'apio raw "icepack {build_name}.txt {build_name}.bin"'
-                ]  
-        return super().build(core, build_name = build_name)
+        if 'arm' in platform.machine():  # --pre-pack can't be used due to apio
+            print(f"Using apio extension with freq {freq} MHz")
+            self.toolchain.nextpnr_build_template = [
+                'apio raw "yosys -q -l {build_name}.rpt {build_name}.ys"',
+                'apio raw "nextpnr-ice40 {pnr_pkg_opts} \
+                    --pcf {build_name}.pcf --json {build_name}.json \
+                    --asc {build_name}.txt --freq '+str(freq)+'"',
+                'apio raw "icepack {build_name}.txt {build_name}.bin"'
+            ]
+        return super().build(core, build_name=build_name)
 
     def upload(self, build_name):
         proc = subprocess.Popen(['icezprog', f'build/{build_name}.bin'],
-                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                 stderr=subprocess.STDOUT)
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
         stdout, stderr = proc.communicate()
         if stderr or ('Failed' in str(stdout)) or ('Error' in str(stdout)):
             raise Exception("Not able to upload bitstream")
--- src/hexastorm/controller.py
+++ src/hexastorm/controller.py
@@ -1,5 +1,5 @@
 import os
-from time import sleep, time
+from time import sleep
 import math
 
 import spidev
@@ -10,24 +10,25 @@ from smbus2 import SMBus
 from hexastorm.core import Scanhead
 import hexastorm.board as board
 
+
 class Machine:
     '''
     class used to control a laser scanner
     '''
     ic_dev_nr = 1
     ic_address = 0x28
-    
+
     def __init__(self):
         self.sh = Scanhead(board.Platform())
         # IC bus used to set power laser
         self.bus = SMBus(self.ic_dev_nr)
         # SPI to sent data to scanner
         self.spi = spidev.SpiDev()
-        self.spi.open(0,0)
+        self.spi.open(0, 0)
         self.spi.max_speed_hz = round(1E6)
         self.spi.cshigh = False
         self.STABLE_TIME = round(Scanhead.VARIABLES['SPINUP_TIME']
-                                    + Scanhead.VARIABLES['STABLE_TIME']+2)
+                                 + Scanhead.VARIABLES['STABLE_TIME']+2)
 
     @property
     def single_facet(self):
@@ -42,7 +43,6 @@ class Machine:
         '''
         assert isinstance(val, bool)
         Scanhead.VARIABLES['SINGLE_FACET'] = val
-
 
     @property
     def single_line(self):
@@ -66,31 +66,34 @@ class Machine:
     @laser_power.setter
     def laser_power(self, val):
         '''
-        set the maximum laser current of driver chip to given value in range [0-255]
-        
-        This does not turn on or off the laser. Laser will be set to this current if pulsed.
-        The laser power can be changed in two ways.
+        set the maximum laser current of driver chip to given value in range
+         [0-255]. This does not turn on or off the laser. Laser will be set
+        to this current if pulsed. The laser power can be changed in two ways.
         First by using one or two channels. Second by setting a value between
-        0-255 at the laser driver chip for the laser current.
-        The laser needs a minimum current. It most likely does not work at low currents.
+        0-255 at the laser driver chip for the laser current. The laser
+         needs a minimum current. It most likely does not work at low currents.
         '''
-        if val < 0 or val > 255: raise Exception('Invalid laser power')
+        if val < 0 or val > 255:
+            raise Exception('Invalid laser power')
         self.bus.write_byte_data(self.ic_address, 0, val)
 
     def bytetostate(self, byte=None):
         'seperate the state and error bits from a byte'
-        if byte is None: byte = self.spi.xfer([self.sh.COMMANDS.STATUS])[0]
-        errors = [int(i) for i in list('{0:0b}'.format(byte&0b11111))]
+        if byte is None:
+            byte = self.spi.xfer([self.sh.COMMANDS.STATUS])[0]
+        errors = [int(i) for i in list('{0:0b}'.format(byte & 0b11111))]
         errors.reverse()
         # pad the list to 5
-        for _ in range(len(errors), 5): errors.append(0)
-        return {'state': byte>>5, 'errorbits': errors}
+        for _ in range(len(errors), 5):
+            errors.append(0)
+        return {'state': byte >> 5, 'errorbits': errors}
 
     def statetobyte(self, errors=[], state=Scanhead.STATES.STOP):
         'create byte correspdonding to a list of errors and a certain state'
         errorstate = 0
-        for error in errors: errorstate += pow(2, error)
-        val = errorstate + (state<<5)
+        for error in errors:
+            errorstate += pow(2, error)
+        val = errorstate + (state << 5)
         return val
 
     def status(self, byte=None, verbose=True):
@@ -99,15 +102,16 @@ class Machine:
 
         returns the machine state and errors as string
         '''
-        #TODO: this will not work if the machine is receiving
+        # TODO: this will not work if the machine is receiving
         bytedict = self.bytetostate(byte)
         state, errors = bytedict['state'], bytedict['errorbits']
-        if state == 255: raise Exception("Check reset pin is high and binary is correct")
+        if state == 255:
+            raise Exception("Check reset pin is high and binary is correct")
         error_string = 'None'
-        if max(errors)>0:
+        if max(errors) > 0:
             error_string = ''
             for idx, val in enumerate(errors):
-                if val>0:
+                if val > 0:
                     error = list(self.sh.ERRORS._asdict())[idx]
                     error_string += error + ' '
         machinestate = list(self.sh.STATES._asdict())[state]
@@ -148,17 +152,17 @@ class Machine:
         byte = (self.spi.xfer([data]))[0]
         state = self.bytetostate(byte)
         trials = 0
-        #TODO: NOTSTABLE can also mean busy --> still needs fix
+        # TODO: NOTSTABLE can also mean busy --> still needs fix
         while (state['errorbits'][self.sh.ERRORS.NOTSTABLE] == 1) and ignore:
             byte = (self.spi.xfer([data]))[0]
             state = self.bytetostate(byte)
             trials += 1
-            if trials>maxtrials:
+            if trials > maxtrials:
                 self.status()
                 self.reset()
-                raise Exception(f"More than {maxtrials} trials required to write to memory")
+                raise Exception(f"More than {maxtrials}\
+                                trials required to write to memory")
         return byte
-
 
     def forcewrite(self, data, maxtrials=1E6):
         byte = (self.spi.xfer([data]))[0]
@@ -167,8 +171,9 @@ class Machine:
         # NOTE: invalids should be detected
         #      this was an issue with an older version of the code
         if (state['errorbits'][self.sh.ERRORS.INVALID] == 1):
-                raise Exception("INVALID DETECTED")
-        while (state['errorbits'][self.sh.ERRORS.MEMFULL] == 1)|(state['errorbits'][self.sh.ERRORS.NOTSTABLE] == 1):
+            raise Exception("INVALID DETECTED")
+        while ((state['errorbits'][self.sh.ERRORS.MEMFULL] == 1)
+               | (state['errorbits'][self.sh.ERRORS.NOTSTABLE] == 1)):
             byte = (self.spi.xfer([data]))[0]
             state = self.bytetostate(byte)
             trials += 1
@@ -176,14 +181,15 @@ class Machine:
                 self.status()
                 self.reset()
                 raise Exception("INVALID DETECTED")
-            if trials>maxtrials:
+            if trials > maxtrials:
                 self.status()
                 self.reset()
-                raise Exception(f"More than {maxtrials} trials required to write to memory")
-    
-    def bittobytelist(self, bitlst, bitorder = 'little'):
+                raise Exception(f"More than {maxtrials} trials\
+                                required to write to memory")
+
+    def bittobytelist(self, bitlst, bitorder='little'):
         '''converts bitlst to bytelst
-        
+
         if bytelst is empty stop command is sent
         '''
         if len(bitlst) == 0:
@@ -204,21 +210,22 @@ class Machine:
         '''
         for _ in range(math.ceil(len(bytelst)/self.sh.CHUNKSIZE)):
             yield self.sh.COMMANDS.WRITE_L
-            for _ in range(self.sh.CHUNKSIZE): 
+            for _ in range(self.sh.CHUNKSIZE):
                 try:
                     yield bytelst.pop()
                 except IndexError:
                     yield 0
 
-    def writeline(self, bitlst, bitorder = 'little'):
+    def writeline(self, bitlst, bitorder='little'):
         '''writes bitlst to memory
-        
+
         if bitlst is empty --> stop command is sent
         '''
-        if len(bitlst)>self.sh.BITSINSCANLINE:
+        if len(bitlst) > self.sh.BITSINSCANLINE:
             raise Exception("Too many bits for a scanline")
         bytelst = self.bittobytelist(bitlst)
-        for byte in self.genwritebytes(bytelst): self.forcewrite(byte)
+        for byte in self.genwritebytes(bytelst):
+            self.forcewrite(byte)
 
     def writepattern(self, pattern):
         '''repeats a pattern so a line is formed and writes to head
@@ -226,7 +233,7 @@ class Machine:
         pattern: list of bits [0] or [1,0,0]
         '''
         line = (pattern*(self.sh.BITSINSCANLINE//len(pattern))
-               + pattern[:self.sh.BITSINSCANLINE%len(pattern)])
+                + pattern[:self.sh.BITSINSCANLINE % len(pattern)])
         self.writeline(line)
 
     def test_photodiode(self):
@@ -237,7 +244,7 @@ class Machine:
         self.busywrite(self.sh.COMMANDS.PHOTODIODETEST)
         sleep(4)
         res = True
-        if self.bytetostate()['state']!=self.sh.STATES.STOP:
+        if self.bytetostate()['state'] != self.sh.STATES.STOP:
             print("Test failed, stopping")
             self.stop()
         else:
@@ -247,14 +254,14 @@ class Machine:
 
     def flash(self, recompile=False, removebuild=False):
         build_name = 'scanhead'
-        plat = board.Platform()   # this object gets depleted, io objects get removed from list if requested
+        plat = board.Platform()   # object is removed from list
         self.sh = Scanhead(plat)  # needs reinit to update settings
         if recompile:
             if os.path.isdir('build'):
                 plat.removebuild()
-            plat.build(freq=50, core = self.sh,
-                       build_name = build_name)
+            plat.build(freq=50, core=self.sh,
+                       build_name=build_name)
         plat.upload(build_name)
         if removebuild:
             plat.removebuild()
-        self.reset()  #TODO: you need to reset, why?!!
+        self.reset()  # TODO: you need to reset, why?!!
