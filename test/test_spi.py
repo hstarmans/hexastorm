87d02f6aba8ea8fa853aa90206dcaafada872ed7
Rik Starmans <info@hexastorm.com>
2021-02-05 09:40:09 +0000

    test case

--- test/test_spi.py
+++ test/test_spi.py
@@ -1,24 +1,20 @@
 import spidev
+from time import sleep
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.no_cs = True
-spi.mode = 0
+spi.no_cs = False
+spi.mode = 1
 spi.max_speed_hz = round(1E6)
-for _ in range(3):
-    spi.cshigh = False
-    print(spi.xfer([10]))
-    print(spi.xfer([10]))
+bts = [210, 222, 230]
+for idx, byte in enumerate(bts):
     spi.cshigh = True
-    print(spi.xfer([10]))
-    print(spi.xfer([10]))
+    if idx != 0:
+        assert current == spi.xfer([byte])[0]
+    else:
+        spi.xfer([byte])
     spi.cshigh = False
-    print(spi.xfer([10]))
-    print(spi.xfer([10]))
-    spi.cshigh = True
-    print(spi.xfer([10]))
-    print(spi.xfer([10]))
-
+    current = byte
 # at the start of transaction
 #  spi.cs is high !
 
