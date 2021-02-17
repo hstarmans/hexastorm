39f54093e6a63a077feb97985b952661c070b4f6
Rik Starmans <info@hexastorm.com>
2021-02-17 09:10:28 +0000

    added luna test

---
+++ test/FPGAG/test_luna.py
@@ -0,0 +1,27 @@
+import unittest
+import struct
+
+from luna.gateware.interface.spi import SPIGatewareTestCase, SPICommandInterface
+from luna.gateware.test.utils import sync_test_case
+
+class Test(SPIGatewareTestCase):
+    FRAGMENT_UNDER_TEST = SPICommandInterface
+
+    def initialize_signals(self):
+        yield self.dut.spi.cs.eq(0)
+
+    @sync_test_case
+    def test_readfrominterface(self):
+        'test if you can receive bytes'
+        yield self.dut.word_to_send.eq(16)
+        for _ in range(4):
+            write_data = [1, 255, 0, 0, 1]
+            read_data = yield from self.spi_exchange_data(write_data)
+            self.assertEqual((yield self.dut.command), 1)
+            self.assertEqual((yield self.dut.word_received), struct.unpack('!I', bytearray(write_data[1:]))[0])
+            # problems: 1. word to send to does not come back
+            #           2. cs magically gets low during transaction 
+            print(read_data)
+
+if __name__ == "__main__":
+    unittest.main()
