696bf03f75445836e09549830a8c125fff48f9bb
Rik Starmans <info@hexastorm.com>
2021-02-17 09:35:08 +0000

    fixed test

--- test/FPGAG/test_luna.py
+++ test/FPGAG/test_luna.py
@@ -1,5 +1,6 @@
 import unittest
 import struct
+import random
 
 from luna.gateware.interface.spi import SPIGatewareTestCase, SPICommandInterface
 from luna.gateware.test.utils import sync_test_case
@@ -13,20 +14,14 @@ class Test(SPIGatewareTestCase):
     @sync_test_case
     def test_readfrominterface(self):
         'test if you can receive bytes'
-        yield self.dut.word_to_send.eq(16)
         for _ in range(4):
+            to_send = random.randint(1,255)
+            yield self.dut.word_to_send.eq(to_send)
             write_data = [1, 255, 0, 0, 1]
             read_data = yield from self.spi_exchange_data(write_data)
             self.assertEqual((yield self.dut.command), 1)
             self.assertEqual((yield self.dut.word_received), struct.unpack('!I', bytearray(write_data[1:]))[0])
-            # problems: 1. word to send to does not come back
-            #           2. cs magically gets low during transaction 
-            print(read_data)
-            # This is printed
-            #    bytearray(b'\x00\x00\x00\x00\x10')
-            #    bytearray(b'\xff\x00\x00\x00\x10')
-            #    bytearray(b'\xff\x00\x00\x00\x10')
-            #    bytearray(b'\xff\x00\x00\x00\x10')
-            # problems: I receive \xff??? and \x10 instead of 16
+            self.assertEqual(to_send, struct.unpack('!I', bytearray(read_data[1:]))[0])
+            # problems: I receive \xff??? for command?
 if __name__ == "__main__":
     unittest.main()
