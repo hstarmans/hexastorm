1805f169b81c86f443144fd6949a29683c3e7dc6
Rik Starmans <info@hexastorm.com>
2021-02-18 10:26:50 +0000

    cleaned up code more

--- src/FPGAG/core.py
+++ src/FPGAG/core.py
@@ -9,7 +9,6 @@ from FPGAG.constants import COMMAND_SIZE, WORD_SIZE, STATE, START_BIT, END_BIT
 from FPGAG.constants import MEMDEPTH, MEMWIDTH, COMMANDS, BYTESINGCODE
 
 
-
 class Divisor(Elaboratable):
     """ Euclidean division with a remainder
     
@@ -77,30 +76,54 @@ class Divisor(Elaboratable):
         return m
 
 class SPIParser(Elaboratable):
-    """ Parses commands over SPI """
+    """ Parses and replies to commands over SPI
+    
+    The following commmands are possible
+      status -- send back state of the peripheriral 
+      start  -- enable execution of gcode
+      stop   -- halt execution of gcode
+      gcode  -- write instruction to FIFO or report memory is full
+
+    I/O signals:
+        I: dispatcherror  -- error while processing stored command from spi 
+        O: execute        -- start processing gcode
+        I/O: Spibus       -- spi bus connected to peripheral
+        O: read_data      -- read data from transactionalizedfifo
+        I: read_commit    -- finalize read transactionalizedfifo
+        I: read_en        -- enable read transactionalizedfifo
+        O: empty          -- transactionalizedfifo is empty
+    """
     def __init__(self):
-        self.dispatcherror = Signal()  # input
-        self.execute = Signal()        # output
-        self.spi = SPIBus()            # I/O
-        # TODO: this is bad practice!!
-        self.fifo = TransactionalizedFIFO(width=MEMWIDTH,
-                                          depth=MEMDEPTH)
+        self.dispatcherror = Signal()  
+        self.execute = Signal()        
+        self.spi = SPIBus()            
+        self.read_data = Signal(MEMWIDTH)
+        self.read_commit = Signal()
+        self.read_en = Signal()
+        self.empty = Signal()
 
     def elaborate(self, platform):
+        m = Module()
+        # Connect SPI
+        spi = synchronize(m, self.spi)
         interface = SPICommandInterface(command_size=COMMAND_SIZE,
                                         word_size=WORD_SIZE)
-        self.interface = interface # to get a handle for debugging
-      
-        m = Module()
-        spi = synchronize(m, self.spi)
-        m.d.comb  += self.interface.spi.connect(spi)
-        m.submodules.interface = interface = self.interface
+        m.d.comb  += interface.spi.connect(spi)
+        m.submodules.interface = interface 
         # Connect fifo
-        m.submodules.fifo = fifo = self.fifo
-        self.fifo = fifo 
+        fifo = TransactionalizedFIFO(width=MEMWIDTH,
+                                     depth=MEMDEPTH)
+        if not platform: # for testing
+            self.fifo = fifo
+        m.submodules.fifo = fifo
+        m.d.comb += [ self.read_data.eq(fifo.read_data),
+                      fifo.read_commit.eq(self.read_commit),
+                      fifo.read_en.eq(self.read_en),
+                      self.empty.eq(fifo.empty)
+                    ]
         # set state
-        state = Signal(COMMAND_SIZE)
-        m.d.sync += [state[STATE.FULL].eq(fifo.space_available>BYTESINGCODE),
+        state = Signal(COMMAND_SIZE) # max is actually word_size
+        m.d.sync += [state[STATE.FULL].eq(fifo.space_available<BYTESINGCODE),
                      state[STATE.DISPATCHERROR].eq(self.dispatcherror)
                     ]
         # Parser
@@ -127,7 +150,7 @@ class SPIParser(Elaboratable):
                             m.next = 'WAIT_COMMAND'
                             m.d.sync += [interface.word_to_send.eq(state)]
                     with m.Elif(interface.command==COMMANDS.STATUS):
-                        m.d.sync += [interface.word_to_send.eq(13)]
+                        m.d.sync += [interface.word_to_send.eq(state)]
                         m.next = 'WAIT_COMMAND'
             with m.State('WAIT_WORD'):
                 with m.If(interface.word_complete):
@@ -144,21 +167,14 @@ class SPIParser(Elaboratable):
                                  fifo.write_commit.eq(1)]
         return m
 
-
 class Core(Elaboratable):
-    """ FPGA core for Beage G. """
+    """ FPGA core for Beage G and top module """
     def __init__(self):
-        self.spi = SPIBus()
+        pass
 
     def elaborate(self, platform):
         m = Module()
-        # Connect Parser
-        spiparser = SPIParser()
-        m.submodules.spiparser = spiparser
-        self.spiparser = spiparser # TODO: remove!
-        spi = synchronize(m, self.spi)
-        m.d.comb  += spiparser.spi.connect(spi)
-        # get directions
+        # Directions
         if platform:
             directions = platform.request("DIRECTIONS")
         else:
@@ -166,30 +182,36 @@ class Core(Elaboratable):
                                  ('diry', 1, DIR_FANOUT),
                                  ('dirz', 1, DIR_FANOUT)])
             self.directions = directions
+        # Connect Parser
+        parser = SPIParser()
+        m.submodules.parser = parser
+        if not platform:
+            self.parser = parser
+        if platform:
+            board_spi = platform.request("debug_spi")
+            spi = synchronize(m, board_spi)
+        else:
+            self.spi = SPIBus()
+            spi = synchronize(m, self.spi)
+        m.d.comb  += parser.spi.connect(spi)
         # Define dispatcher
-        fifo = spiparser.fifo
-        error = Signal()
-        enabled = Signal()
-        m.d.sync += [spiparser.dispatcherror.eq(error),
-                     enabled.eq(spiparser.execute)]
-        bytesreceived = Signal(range(BYTESINGCODE))
         with m.FSM(reset='RESET', name='dispatcher'):
-            m.d.sync += [fifo.read_commit.eq(0)]
+            m.d.sync += [parser.read_commit.eq(0)]
             with m.State('RESET'):
                 m.next = 'WAIT_COMMAND'
             with m.State('WAIT_COMMAND'):
-                with m.If((fifo.empty == 0)&(enabled==1)):
-                    m.d.sync += [fifo.read_en.eq(1)]
+                with m.If((parser.empty == 0)&(parser.execute==1)):
+                    m.d.sync += [parser.read_en.eq(1)]
                     m.next = 'PARSEHEAD'
             with m.State('PARSEHEAD'):
-                with m.If(fifo.read_data[-8:] == COMMANDS.GCODE):
-                    m.d.sync += [directions.eq(fifo.read_data[-END_BIT['DIRECTION']\
+                with m.If(parser.read_data[-8:] == COMMANDS.GCODE):
+                    m.d.sync += [directions.eq(parser.read_data[-END_BIT['DIRECTION']\
                                                :-START_BIT['DIRECTION']]),
-                                 fifo.read_commit.eq(1)]
+                                 parser.read_commit.eq(1)]
                     m.next = 'WAIT_COMMAND'
                 with m.Else():
                     # NOTE: system never recovers user must reset
-                    m.d.sync += [error.eq(1)]
+                    m.d.sync += [parser.dispatcherror.eq(1)]
         return m
 
 # Overview:
@@ -199,7 +221,5 @@ class Core(Elaboratable):
 #  -- SPI parser (basically an extension of SPI command interface)
 #  -- Dispatcher --> dispatches signals to actual hardware
 
-
-#  if you write the wrong command --> you receive an error
-
-#   --> als ik te vaak schrijf naar bord dan raakt ie vol
+# -- verify you can trigger memory full
+# -- write a controller class
--- test/FPGAG/test_virtual.py
+++ test/FPGAG/test_virtual.py
@@ -1,19 +1,18 @@
 import unittest
 from struct import pack, unpack
 
-
 from luna.gateware.interface.spi import SPIGatewareTestCase
 from luna.gateware.test.utils import sync_test_case
 
-from FPGAG.core import Core
+from FPGAG.core import Core, SPIParser
 from FPGAG.constants import COMMANDS, MEMDEPTH, BYTESINGCODE, WORD_SIZE
 from FPGAG.constants import COMMAND_SIZE
 
-class Test(SPIGatewareTestCase):
-    FRAGMENT_UNDER_TEST = Core
+
+class TestParser(SPIGatewareTestCase):
+    FRAGMENT_UNDER_TEST = SPIParser
 
     def initialize_signals(self):
-        self.dut.fifo = self.dut.spiparser.fifo
         yield self.dut.spi.cs.eq(0)
 
     def write_command(self, data):
@@ -24,20 +23,33 @@ class Test(SPIGatewareTestCase):
 
     @sync_test_case
     def test_writegcode(self):
-        'write directions to the motor pins'
-        self.assertEqual((yield self.dut.fifo.empty), 1)
-        self.assertEqual((yield self.dut.fifo.space_available), MEMDEPTH)
+        'write GCODE and verify fifo gets full'
+        self.assertEqual((yield self.dut.empty), 1)
         # write GCODE command with data
         bytes_sent = 0
-        while bytes_sent!=BYTESINGCODE:
+        while bytes_sent!= BYTESINGCODE:
             writedata = [COMMANDS.GCODE, 1, 2, 3, 4]
             bytes_sent += 4
             _ = yield from self.spi_exchange_data(writedata)
-            self.assertEqual((yield self.dut.fifo.empty), 1)
-        while (yield self.dut.fifo.write_commit) == 0:
+        while (yield self.dut.empty) == 1:
             yield
-        self.assertEqual((yield self.dut.fifo.space_available), MEMDEPTH-BYTESINGCODE/(WORD_SIZE/8))
-        self.assertEqual((yield self.dut.fifo.empty), 1)
+        # Instruction ready
+        self.assertEqual((yield self.dut.empty), 0)
+        self.assertEqual((yield self.dut.fifo.space_available),
+                         MEMDEPTH-BYTESINGCODE/(WORD_SIZE/8))
+
+class TestCore(SPIGatewareTestCase):
+    FRAGMENT_UNDER_TEST = Core
+
+    def initialize_signals(self):
+        yield self.dut.spi.cs.eq(0)
+
+    # NOTE REFACTOR
+    def write_command(self, data):
+        'convenience function for writing command to controller'
+        assert len(data) == (WORD_SIZE+COMMAND_SIZE)/8
+        read_data = yield from self.spi_exchange_data(data)
+        return unpack('!I', read_data[1:])[0]
 
     @sync_test_case
     def test_setdirection(self):
@@ -52,14 +64,14 @@ class Test(SPIGatewareTestCase):
             bytes_sent += 4
             _ = yield from self.spi_exchange_data(writedata)
         # wait for data to be committed
-        while (yield self.dut.fifo.write_commit) == 0:
+        while (yield self.dut.parser.empty) == 1:
             yield
         yield
         # write execute
-        writedata = [COMMANDS.START, COMMANDS.GCODE, 255, 0, 0]
+        writedata = [COMMANDS.START, 0, 0, 0, 0]
         _ = yield from self.spi_exchange_data(writedata)
         # data should now be parsed and empty become 1
-        while (yield self.dut.fifo.empty) == 0:
+        while (yield self.dut.parser.empty) == 0:
             yield
         self.assertEqual((yield self.dut.directions.dirx), 1)
         self.assertEqual((yield self.dut.directions.diry), 1)
@@ -75,28 +87,25 @@ class Test(SPIGatewareTestCase):
             bytes_sent += 4
             _ = yield from self.spi_exchange_data(writedata)
         # wait for data to be committed
-        while (yield self.dut.fifo.write_commit) == 0:
+        while (yield self.dut.parser.empty) == 1:
             yield
-        yield
-        # write execute
-        writedata = [COMMANDS.START, COMMANDS.GCODE, 255, 0, 0]
+        # execute code
+        writedata = [COMMANDS.START, 0, 0, 0, 0]
         _ = yield from self.spi_exchange_data(writedata)
         # data should now be parsed and empty become 1
-        while (yield self.dut.fifo.empty) == 0:
+        while (yield self.dut.parser.empty) == 0:
             yield
         # 2 clocks needed for error to propagate
         yield
         yield
-        self.assertEqual((yield self.dut.spiparser.dispatcherror), 1)
+        self.assertEqual((yield self.dut.parser.dispatcherror), 1)
         # let's request the status
         bytes_sent = 0
-        # TODO: you have jumping rings!!
         while bytes_sent<BYTESINGCODE:
             writedata = [COMMANDS.STATUS, 0, 0, 0, 0]
             bytes_sent += 4
             read_data = yield from self.write_command(writedata)
-            print(read_data)
-
+            self.assertEqual(read_data, 2)
 
 if __name__ == "__main__":
     unittest.main()
