b01a6ab26af6fc789c2fba99dc76f8f47a288b1f
Rik Starmans <info@hexastorm.com>
2021-03-30 15:39:06 +0100

    fixed example

--- test/debug_spi/debug_spi.py
+++ test/debug_spi/debug_spi.py
@@ -1,47 +1,39 @@
 """ debug spi
 
 Only got it working for older version of luna
-git checkout hw-r0.2-154-ge49508f
+git checkout f54de01c9dc13aeca5d20734a9c103227cb9603f
+upstream has been notified https://github.com/greatscottgadgets/luna/issues/101
 """
-#!/usr/bin/env python3
-#
-# This file is part of LUNA.
-#
-# Copyright (c) 2020 Great Scott Gadgets <info@greatscottgadgets.com>
-# SPDX-License-Identifier: BSD-3-Clause
+from nmigen import Elaboratable, Module
 
-from nmigen import Signal, Elaboratable, Module
-from nmigen.lib.cdc import FFSynchronizer
+from FPGAG.platforms import Firestarter
 
 from luna import top_level_cli
 from luna.gateware.utils.cdc import synchronize
-from luna.gateware.interface.spi import SPIDeviceInterface, SPIBus
+from luna.gateware.interface.spi import SPIDeviceInterface
 
 
 class DebugSPIExample(Elaboratable):
-    """ Hardware meant to demonstrate use of the Debug Controller's SPI interface. """
-
+    """ Hardware meant to demonstrate use of
+        the Debug Controller's SPI interface copied from Luna """
 
     def __init__(self):
 
         # Base ourselves around an SPI command interface.
         self.interface = SPIDeviceInterface(clock_phase=1)
 
-
     def elaborate(self, platform):
         m = Module()
         board_spi = platform.request("debug_spi")
 
-        # Use our command interface.
+        # Use command interface.
         m.submodules.interface = self.interface
 
-        #
-        # Synchronize and connect our SPI.
-        #
+        # Synchronize and connect SPI.
         spi = synchronize(m, board_spi)
-        m.d.comb  += self.interface.spi.connect(spi)
+        m.d.comb += self.interface.spi.connect(spi)
 
-        # Turn on a single LED, just to show something's running.
+        # Turn on a single LED, to show something's running.
         led = platform.request('led', 0)
         m.d.comb += led.eq(1)
 
@@ -52,4 +44,5 @@ class DebugSPIExample(Elaboratable):
 
 
 if __name__ == "__main__":
-    top_level_cli(DebugSPIExample)
+    platform = Firestarter()
+    platform.build(DebugSPIExample(), do_program=True, verbose=True)
