33ad3e29317d63b62e0a6fe9f5a96722d97a80ed
Rik Starmans <hstarmans@hexastorm.com>
2022-12-20 12:13:16 +0100

    added controller to driver and stability measurement

--- subprojects/custommotor/driver.py
+++ subprojects/custommotor/driver.py
@@ -30,8 +30,8 @@ class Driver(Elaboratable):
 
     def elaborate(self, platform):
         m = Module()
-        state = Signal(3)
-        stateleds = Signal(3)
+        motorstate = Signal(3)
+        hallstate = Signal().like(motorstate)
 
         def get_all_resources(name):
             resources = []
@@ -49,16 +49,15 @@ class Driver(Elaboratable):
             leds = [res.o for res in get_all_resources("led")]
             bldc = platform.request("bldc")
             m.d.comb += self.spi.connect(spi2)
-            m.d.comb += stateleds.eq(Cat(~bldc.sensor0,
+            m.d.comb += hallstate.eq(Cat(~bldc.sensor0,
                                          ~bldc.sensor1,
                                          ~bldc.sensor2))
+            m.d.comb += leds[0].eq(hallstate[0])
+            m.d.comb += leds[1].eq(hallstate[1])
+            m.d.comb += leds[2].eq(hallstate[2])
         else:
             platform = self.platform
             bldc = platform.bldc
-
-        max_delay = 1000
-        delay = Signal(range(max_delay))
-        stateledsold = Signal.like(stateleds)
 
         def get_cycletime(frequency):
             return int(platform.laser_var['CRYSTAL_HZ']/(frequency*12))
@@ -76,65 +75,66 @@ class Driver(Elaboratable):
             with m.State('ROTATION'):
                 with m.If(mtrpulsecntr == start_statetime):
                     m.d.sync += mtrpulsecntr.eq(0)
-                    with m.If(state == 5):
-                        m.d.sync += state.eq(0)
+                    with m.If(motorstate == 5):
+                        m.d.sync += motorstate.eq(0)
                     with m.Else():
-                        m.d.sync += state.eq(state+1)
+                        m.d.sync += motorstate.eq(motorstate+1)
                 with m.Else():
                     m.d.sync += mtrpulsecntr.eq(mtrpulsecntr+1)
                 with m.If(rotating == 1):
                     m.next = 'HALL'
             with m.State('HALL'):
-                with m.If(stateleds == 1):
-                    m.d.sync += state.eq(0)
-                with m.Elif(stateleds == 2):
-                    m.d.sync += state.eq(2)
-                with m.Elif(stateleds == 3):
-                    m.d.sync += state.eq(1)
-                with m.Elif(stateleds == 4):
-                    m.d.sync += state.eq(4)
-                with m.Elif(stateleds == 5):
-                    m.d.sync += state.eq(5)
-                with m.Elif(stateleds == 6):
-                    m.d.sync += state.eq(3)
+                with m.If(hallstate == 1):
+                    m.d.sync += motorstate.eq(0)
+                with m.Elif(hallstate == 2):
+                    m.d.sync += motorstate.eq(2)
+                with m.Elif(hallstate == 3):
+                    m.d.sync += motorstate.eq(1)
+                with m.Elif(hallstate == 4):
+                    m.d.sync += motorstate.eq(4)
+                with m.Elif(hallstate == 5):
+                    m.d.sync += motorstate.eq(5)
+                with m.Elif(hallstate == 6):
+                    m.d.sync += motorstate.eq(3)
                 # hall off and all hall on are filtered
                 # i.e. cases 0 en 7
                 with m.If(rotating == 0):
                     m.next = 'ROTATION'
 
         statecounter = Signal(range(transitions_cycle+1))
-        stateledsoldold = Signal().like(stateleds)
-        stateledssuperold = Signal().like(stateleds)
-        
-        duty_step = max_delay//100
-        
-        m.d.comb += leds[0].eq(rotating)
+        stateold = Signal.like(hallstate)
+        stateoldold = Signal().like(hallstate)
+        statesuperold = Signal().like(hallstate)
 
         # Cycle time measurement
-        with m.If((stateleds != stateledsold) &
-                  (stateleds != stateledsoldold) &
-                  (stateleds != stateledssuperold)):
-            m.d.sync += [stateledsold.eq(stateleds),
-                         stateledsoldold.eq(stateledsold),
-                         stateledssuperold.eq(stateledsoldold)]
+        with m.If((hallstate != stateold) &
+                  (hallstate != stateoldold) &
+                  (hallstate != statesuperold)):
+            m.d.sync += [stateold.eq(hallstate),
+                         # this can blow up counter
+                         hallpulsecntr.eq(hallpulsecntr+1),
+                         stateoldold.eq(stateold),
+                         statesuperold.eq(stateoldold)]
             with m.If(statecounter > transitions_cycle):
                 m.d.sync += [statecounter.eq(0),
                              hallpulsecntr.eq(0)]
                 # store measurerement
+                # only if the measurement is reasonable
+                # if there are too few ticks --> not reasonable
                 with m.If(hallpulsecntr > (get_cycletime(100)*12)):
                     # rotating so accelerate
                     m.d.sync += [rotationtime.eq(hallpulsecntr),
                                  rotating.eq(1)]
-                with m.Else():
-                    # not rotating
-                    m.d.sync += [rotationtime.eq(0),
-                                 rotating.eq(0)]
+                # not sure what to do with unreasonable ticks
+                # if speed is too high it is ignored
             with m.Else():
                 m.d.sync += statecounter.eq(statecounter+1)
         with m.Else():
+            # there should already have been a reset
+            # due to statecounter is 12
+            # ergo you r not rotating
             with m.If(hallpulsecntr >= int(start_statetime*14)):
-                # not rotating
-                m.d.sync += leds[1].eq(~leds[1])
+                #m.d.sync += leds[1].eq(~leds[1])
                 m.d.sync += [hallpulsecntr.eq(0),
                              rotating.eq(0),
                              rotationtime.eq(0)]
@@ -148,65 +148,85 @@ class Driver(Elaboratable):
         m.submodules.interf = interf
         m.d.sync += interf.word_to_send.eq(rotationtime)
 
+        # PID controller
+        # Ideal is controlling both voltage and current
+        # Speed 
         off = Signal()
-#         duty = Signal(range(max_delay))
+        # RPM --> ticks
+        target = int(round(13.56E6/(2000/60)))
+        max_delay = 1000
+        duty_step = max_delay//100
+        # specifies current delay (counter)
+        delay = Signal(range(max_delay))
+        # specificies where you are in duty cycle (counter)
+        duty = Signal(range(max_delay))
+        
+        step = 1 #max_delay//100
+        with m.If(rotating == 0):
+            m.d.sync += delay.eq(0)
+        # CASE: rotating too slow
+        with m.Elif((rotationtime > target) & (delay >= step)):
+            m.d.sync += delay.eq(delay - step)
+        # CASE: rotating too fast
+        with m.Elif((rotationtime < target) & (delay < (max_delay-step)) & (delay<int(0.95*max_delay))):
+            m.d.sync += delay.eq(delay+step)
 
-#         # Duty timer
-#         with m.If(duty < max_delay):
-#             m.d.sync += duty.eq(0)
-#         with m.Else():
-#             m.d.sync += duty.eq(duty+1)
+        # Duty timer
+        with m.If(duty < max_delay):
+            m.d.sync += duty.eq(0)
+        with m.Else():
+            m.d.sync += duty.eq(duty+1)
 
-#         # Motor On / Off
-#         with m.If(duty < delay):
-#             m.d.sync += off.eq(1)
-#         with m.Else():
-#             m.d.sync += off.eq(0)
+        # Motor On / Off
+        with m.If(duty < delay):
+            m.d.sync += off.eq(1)
+        with m.Else():
+            m.d.sync += off.eq(0)
 
         # https://www.mathworks.com/help/mcb/ref/sixstepcommutation.html
-        with m.If(off):
+        with m.If(off & (~rotating)):
             m.d.comb += [bldc.uL.eq(0),
                          bldc.uH.eq(0),
                          bldc.vL.eq(0),
                          bldc.vH.eq(0),
                          bldc.wL.eq(0),
                          bldc.wH.eq(0)]
-        with m.Elif(state == 0):  # V --> W, 001
+        with m.Elif(motorstate == 0):  # V --> W, 001
             m.d.comb += [bldc.uL.eq(0),
                          bldc.uH.eq(0),
                          bldc.vL.eq(0),
                          bldc.vH.eq(1),
                          bldc.wL.eq(1),
                          bldc.wH.eq(0)]
-        with m.Elif(state == 1):  # V --> U, 011
+        with m.Elif(motorstate == 1):  # V --> U, 011
             m.d.comb += [bldc.uL.eq(1),
                          bldc.uH.eq(0),
                          bldc.vL.eq(0),
                          bldc.vH.eq(1),
                          bldc.wL.eq(0),
                          bldc.wH.eq(0)]
-        with m.Elif(state == 2):  # W --> U, 010
+        with m.Elif(motorstate == 2):  # W --> U, 010
             m.d.comb += [bldc.uL.eq(1),
                          bldc.uH.eq(0),
                          bldc.vL.eq(0),
                          bldc.vH.eq(0),
                          bldc.wL.eq(0),
                          bldc.wH.eq(1)]
-        with m.Elif(state == 3):  # W --> V, 110
+        with m.Elif(motorstate == 3):  # W --> V, 110
             m.d.comb += [bldc.uL.eq(0),
                          bldc.uH.eq(0),
                          bldc.vL.eq(1),
                          bldc.vH.eq(0),
                          bldc.wL.eq(0),
                          bldc.wH.eq(1)]
-        with m.Elif(state == 4):  # U --> V, 100
+        with m.Elif(motorstate == 4):  # U --> V, 100
             m.d.comb += [bldc.uL.eq(0),
                          bldc.uH.eq(1),
                          bldc.vL.eq(1),
                          bldc.vH.eq(0),
                          bldc.wL.eq(0),
                          bldc.wH.eq(0)]
-        with m.Elif(state == 5):  # U --> W, 101
+        with m.Elif(motorstate == 5):  # U --> W, 101
             m.d.comb += [bldc.uL.eq(0),
                          bldc.uH.eq(1),
                          bldc.vL.eq(0),
--- subprojects/custommotor/test_electrical.py
+++ subprojects/custommotor/test_electrical.py
@@ -3,8 +3,11 @@ from time import sleep, time_ns, time
 import spidev
 from copy import deepcopy
 import csv
+import subprocess
 
 from gpiozero import LED
+import numpy as np
+
 from platforms import Firestarter
 from driver import Driver
 from struct import unpack
@@ -12,21 +15,25 @@ from struct import unpack
 
 class Base(unittest.TestCase):
     @classmethod
-    def setUpClass(cls, build=True):
-        platform = Firestarter()
-        cls.reset_pin = LED(platform.reset_pin)
-        cls.chip_select = LED(platform.chip_select)
+    def setUpClass(cls, build=False):
+        '''programs board
+
+        if build is False FPGA is only reset
+        and not flashed.
+        '''
+        cls.platform = Firestarter()
+        cls.reset_pin = LED(cls.platform.reset_pin)
+        cls.chip_select = LED(cls.platform.chip_select)
 
         # SPI to sent data to scanner
         cls.spi = spidev.SpiDev()
-        cls.spi.open(*platform.spi_dev)
+        cls.spi.open(*cls.platform.spi_dev)
         cls.spi.mode = 1
         cls.spi.max_speed_hz = round(1E6)
         if build:
             print("Building and programming board")
-            platform = Firestarter()
-            platform.build(Driver(Firestarter(), top=True),
-                           do_program=True, verbose=True)
+            cls.platform.build(Driver(Firestarter(), top=True),
+                               do_program=True, verbose=True)
         else:
             cls.reset_pin.off()
             sleep(1)
@@ -34,39 +41,61 @@ class Base(unittest.TestCase):
             sleep(1)
             print("Not programming board")
 
-    def test_readfreq(self):
+    def read_freq(self):
+        '''reads rotor frequency in ticks via SPI''' 
+        data = [1]*5  # random bytes
+        self.chip_select.off()
+        # spidev changes values passed to it
+        datachanged = deepcopy(data)
+        response = bytearray(self.spi.xfer(datachanged))
+        self.chip_select.on()
+        ticks = unpack(">I", bytearray(response[1:]))[0]
+        if ticks != 0:
+            freq = (13.56E6/ticks)
+        else:
+            freq = None
+        return freq
+
+    def test_readfreq(self, delay=1):
+        '''turns on the motor board and retrieves the rotor frequency
+
+        Method runs for ever, can be interrupted with keyboard interrupt.
+        '''
         with open(str(int(time()))+'.csv', 'w') as csvfile:
-            while True:
-                data = [1]*5  # random bytes
-                self.chip_select.off()
-                # spidev changes values passed to it
-                datachanged = deepcopy(data)
-                response = bytearray(self.spi.xfer(datachanged))
-                self.chip_select.on()
-                ticks = unpack(">I", bytearray(response[1:]))[0]
-                writer = csv.writer(csvfile)
-                writer.writerow([time_ns(), ticks])
-                if ticks != 0:
-                    freq = (12E6/(ticks*2))
-                    print(f"Freq is {freq:.2f} and RPM is {freq*60:.2f}")
-                else:
-                    print(f"Invalid measured 0")
-                sleep(1)
-#     def test_sensor(self):
-#         # je kunt de SDO pin veranderen vanuit FPGA
-#         # je leest verschil met multimeter op pin
-#         # je kunt ook de sensor pin veranderen van uit fpga
-#         print('Reading sensor')
-#         while True:
-#             sleep(1)
-#             print(self.sensor_pin0.value,
-#                   self.sensor_pin1.value, 
-#                   self.sensor_pin2.value)
-#     def test_motor(self):
-#         self.enable_pin.on()
-#         while True:
-#             sleep(1)
-#             print(self.sensor_pin.value)
+            lst = []
+            try:
+                cntr = 0
+                while True:
+                    cntr += 1
+                    if cntr == 120:
+                        print("Starting measurement")
+                    if cntr == 180:
+                        print("Measurement finished")
+                        break
+                    freq = self.read_freq()
+                    if cntr > 120:
+                        if freq:
+                            lst.append(freq)
+                    writer = csv.writer(csvfile)
+                    writer.writerow([time_ns(), freq])
+                    if freq:
+                        print(f"Freq is {freq:.2f} and RPM is {freq*60:.2f}")
+                    else:
+                        print(f"Invalid measured 0")
+                    sleep(delay)
+            except KeyboardInterrupt:
+                pass
+            finally:
+                self.reset_pin.off()
+                self.reset_pin.close()
+                # gpiozero cleans up pins
+                # this ensures pin is kept off
+                command = subprocess.run(["raspi-gpio", "set",
+                                          str(self.platform.reset_pin),
+                                          "op", "dl"])
+                ar = np.asarray(lst)
+                print(f'Mean {np.mean(ar):.4f} and std {np.std(ar):.4f}')
+                print('Interrupted, exiting')
 
 
 if __name__ == "__main__":
